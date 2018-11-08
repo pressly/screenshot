8ea65a9e0f1890ce632016adeff9608d99ef89c8
1vn <ivan@ivanzhang.ca>
2018-11-08 15:00:00 -0500

    add resolution parsing

--- server/server.go
+++ server/server.go
@@ -2,13 +2,42 @@ package server
 
 import (
 	"context"
-	"io/ioutil"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
 
+	"github.com/chromedp/cdproto/page"
+	"github.com/pkg/errors"
 	"github.com/pressly/screenshot/lib/headless"
 	pb "github.com/pressly/screenshot/rpc/screenshot"
 )
 
 type Server struct{}
+
+type resolution struct {
+	x float64
+	y float64
+}
+
+func parseResolution(s string) (*resolution, error) {
+	parts := strings.Split(s, "x")
+	if len(parts) < 3 {
+		return nil, errors.New(fmt.Sprintf("failed to parse resolution: %s", s))
+	}
+	x, err := strconv.ParseFloat(parts[0], 64)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to parse width: %s", parts[0])
+	}
+	y, err := strconv.ParseFloat(parts[2], 64)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to parse height: %s", parts[0])
+	}
+	return &resolution{
+		x: x,
+		y: y,
+	}, nil
+}
 
 func (s *Server) Image(ctx context.Context, req *pb.RequestImage) (*pb.Resp, error) {
 	// create context
@@ -21,17 +50,29 @@ func (s *Server) Image(ctx context.Context, req *pb.RequestImage) (*pb.Resp, err
 	}
 	defer c.Teardown()
 
-	r, err := c.NewImage(ctx, req.Url)
+	window, err := parseResolution(req.Window)
 	if err != nil {
+		return nil, errors.Wrapf(err, "failed to parse window: %s", req.Window)
+	}
+
+	targetID := fmt.Sprintf("pressly/screenshot/%d", rand.Int63n(100))
+	target := c.NewTarget(&targetID)
+	if err := c.Run(ctx, target); err != nil {
 		return nil, err
 	}
 
-	by, err := ioutil.ReadAll(r)
-	if err != nil {
-		return nil, err
+	exec := c.GetHandlerByID(targetID)
+	screenshotParams := page.CaptureScreenshot()
+	screenshotParams.Clip = &page.Viewport{
+		X:      float64(req.X),
+		Y:      float64(req.Y),
+		Width:  window.x,
+		Height: window.y,
 	}
+
+	image, err := screenshotParams.Do(ctx, exec)
 
 	return &pb.Resp{
-		Resp: by,
+		Resp: image,
 	}, nil
 }
